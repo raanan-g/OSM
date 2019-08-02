1d164524dced176ef641ef3b91bd1f84667cde94
raanan-g <41341275+raanan-g@users.noreply.github.com>
2019-08-02 09:44:15 -0400

    Add files via upload

---
+++ geofabrik.py
@@ -0,0 +1,16 @@
+﻿# OSM Data Pipeline
+
+# Step 1: Download each region's pbf file from geofrabrik.de
+import pandas as pd
+import urllib.request
+
+regs = 'us-midwest','us-northeast','us-pacific','us-south','us-west'
+url = "http://download.geofabrik.de/north-america/"
+
+for item in reg:
+	link = url+reg+'-latest.osm.pbf'
+	path = reg+'.pbf'
+	urllib.request.urlretrieve(link, path)
+
+
+
